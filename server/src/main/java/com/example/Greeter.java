4c7ff09d7a2be203c86715f4fab705a7940bb29c
stopangin <xxx@xxx.com>
2017-10-01 08:27:30 +0200

    updated comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,20 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter() {
 
-  }
+    }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    /**
+     * Does the greeting.
+     *
+     * @param someone - who we want to send greeting to.
+     * @return greeting.
+     */
+    public String greet(final String someone) {
+        return String.format("Hello, %s!", someone);
+    }
 }
